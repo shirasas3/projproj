f573ed8322083152456f0b6ef92b5e5cf3c8337f
shirasas3 <shirasasson7@gmail.com>
2026-09-16 08:43:07 +0300

    tries

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/projproj/',   // 👈 replace "repo-name" with your actual GitHub repo name
   plugins: [react()],
 })
